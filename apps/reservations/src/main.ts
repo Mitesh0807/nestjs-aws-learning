066901164ed5ca7ff5f828dad5d543339288b54b
Mitesh savaliya <msavaliya@plusonex.com>
2024-03-11 10:50:18 +0530

    pino logger intial setup with abstract module

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from "nestjs-pino";
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useLogger(app.get(''))
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,23 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './reservations/models/reservation.schema';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }])],
+  imports: [DatabaseModule, DatabaseModule.forFeature([{ name: ReservationDocument.name, schema: ReservationSchema }]), LoggerModule.forRoot(
+    {
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            singleLine: true
+          }
+        }
+      }
+    }
+  ), LoggerModule
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 
