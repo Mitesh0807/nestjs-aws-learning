a1a8aaa4de19a963f44730e701f1fbe8a35ce1f4
Mitesh savaliya <msavaliya@plusonex.com>
2024-03-12 10:41:18 +0530

    config service resolving hmr
    Config service Resolve and working on hmr for docker build not working not able to reproduce solution using tsconfig

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -5,7 +5,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
 import { LoggerModule } from '@app/common';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     UsersModule,
@@ -18,6 +18,12 @@ import { LoggerModule } from '@app/common';
         JWT_EXPIRATION: Joi.string().required(),
         HTTP_PORT: Joi.number().required(),
         TCP_PORT: Joi.number().required(),
+      }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRATION },
       }),
     }),
   ],
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from "nestjs-pino";
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
+  console.log("ef https://downloadlynet.ir/?s=NestJS+Fundamentals+Course ")
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  app.use(cookieParser());
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
