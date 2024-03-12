7965bb3785368a6331dd43019af279fae97e38f0
Mitesh savaliya <miteshsavaliya7@gmail.com>
2024-03-12 15:51:06 +0530

    implementaion of local strategy and jwt strategy setup

---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { User } from '@app/common';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<User> {
+    return this.authService.validateUser(username, password);
+  }
+}
