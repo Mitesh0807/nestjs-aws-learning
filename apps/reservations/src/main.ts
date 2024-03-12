e379ea6f280b9bf98d8b2b639e3f671934dfb8d8
Mitesh savaliya <miteshsavaliya7@gmail.com>
2024-03-12 11:15:12 +0530

    commit after resolving hmr in wsl

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -5,7 +5,6 @@ import { Logger } from "nestjs-pino";
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
-  console.log("ef https://downloadlynet.ir/?s=NestJS+Fundamentals+Course ")
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
